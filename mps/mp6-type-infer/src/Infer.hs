8dcce5f38deea5f654f77bebf6a20202a6240bd7
Jiatuo Zou <jiatuoz2@illinois.edu>
2022-07-31 20:58:16 -0500

    finished mp

--- mps/mp6-type-infer/src/Infer.hs
+++ mps/mp6-type-infer/src/Infer.hs
@@ -46,7 +46,71 @@ unify constraints =
   {- question 4: type inference -}
 
 infer :: TypeEnv -> Exp -> Infer MonoTy
-infer env exp = undefined
+infer env (ConstExp c) = freshInst $ constTySig c
+
+infer env (VarExp s) = case H.lookup s env of
+  Nothing -> throwError $ LookupError s
+  Just a -> freshInst a
+
+infer env (LetExp x e1 e2) = do 
+  (tau1, constraints) <- listen $ infer env e1
+  subst <- unify constraints
+  tau <- return $ apply subst tau1
+  tau2 <- infer (insert x (gen env tau) env) e2
+  return $ tau2
+
+infer env (BinOpExp binop e1 e2) = do
+  (tau1, c1) <- listen $ infer env e1
+  (tau2, c2) <- listen $ infer env e2
+  tau <- freshInst $ binopTySig binop
+  ntau <- freshTau
+  constrain (funTy tau1 (funTy tau2 ntau)) tau
+  subst <- unify ([(funTy tau1 (funTy tau2 ntau)) :~: tau] ++ c1 ++ c2)
+  return $ apply subst ntau
+
+infer env (MonOpExp monop e) = do
+  (tau1, c1) <- listen $ infer env e
+  tau <- freshInst $ monopTySig monop
+  ntau <- freshTau
+  constrain (funTy tau1 ntau) tau
+  subst <- unify ([(funTy tau1 ntau) :~: tau] ++ c1)
+  return $ apply subst ntau
+
+infer env (IfExp e1 e2 e3) = do
+  (tau1, c1) <- listen $ infer env e1
+  (tau2, c2) <- listen $ infer env e2
+  (tau3, c3) <- listen $ infer env e3
+  constrain tau1 boolTy
+  constrain tau2 tau3
+  subst <- unify ([tau1 :~: boolTy, tau2 :~: tau3] ++ c1 ++ c2 ++ c3)
+  return $ apply subst tau2 
+
+infer env (FunExp v e) = do
+  tau1 <- freshTau
+  (tau2, c) <- listen $ infer (insert v (Forall [] tau1) env) e
+  subst <- unify c
+  return $ funTy (apply subst tau1) tau2
+
+infer env (AppExp f a) = do
+  (tau1, c1) <- listen $ infer env f
+  (tau2, c2) <- listen $ infer env a
+  tau <- freshTau
+  constrain tau1 (funTy tau2 tau)
+  subst <- unify ([tau1 :~: (funTy tau2 tau)] ++ c1 ++ c2)
+  return $ apply subst tau
+
+infer env (LetRecExp f x e1 e) = do
+  tau1 <- freshTau
+  tau2 <- freshTau
+  (tau3, c1) <- listen $ infer ((fromList [(f, Forall [] (funTy tau1 tau2)), (x, Forall [] tau1)]) `H.union` env) e1
+  constrain tau2 tau3
+  subst1 <- unify ((tau2 :~: tau3):c1)
+  ntau <- return $ apply subst1 (funTy tau1 tau2)
+  (tau, c2) <- listen $ infer (insert f (gen env ntau) env) e
+  subst2 <- unify (c2 ++ c1)
+  return $ apply subst2 tau
+
+
 
 inferInit :: TypeEnv -> Exp -> Infer PolyTy
 inferInit env e = do
